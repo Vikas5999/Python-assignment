fae2e2c23d7090404aff1bca0ee3edb203fe2bbe
Vikas5999 <vikasdon95@gmail.com>
2026-05-04 15:39:49 +0530

    first commit

---
+++ 01-number-swapping.py
@@ -0,0 +1,12 @@
+#Switch values of two integer
+n1=20
+n2=30
+print(f"Before Swapping value of n1:{n1}") #f stands for string formation
+print(f"Before Swapping value of n2:{n1}")
+
+x=n1
+n1=n2
+n2=x
+print(" ")
+print(f"After Swapping value of n2:{n1}")
+print(f"After Swapping value of n2:{n1}")
