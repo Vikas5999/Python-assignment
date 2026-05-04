cccb3f6a189f71761569cbbef050d09f8c4f581f
Vikas5999 <vikasdon95@gmail.com>
2026-05-04 15:52:45 +0530

    first commit

---
+++ 02-Update-balance.py
@@ -0,0 +1,12 @@
+#Switch values of two strings (characters)
+#Input:
+char1 = "hello"
+char2 = "java"
+print(f"before swap char1 = {char1}")
+print(f"before swap char2 = {char2}")
+temp=char1
+char1=char2
+char2=temp
+print(f"after swap char1 = {char1}")
+print(f"after swap char2 = {char2}")
+
