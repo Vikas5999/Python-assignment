2986e7de161748aece62c4cd8395834006f6f9f9
Vikas5999 <vikasdon95@gmail.com>
2026-05-05 11:06:27 +0530

    update file

---
+++ 03-swapint-str.py
@@ -0,0 +1,16 @@
+# 3. Switch one string value with one integer value 
+  
+n1 = 200,  
+char2 = "java" 
+print(f"Before swap n1= {n1}")
+print(f"Before swap char2= {char2}")
+
+temp = n1
+n1=char2
+char2=temp
+print(f"After swap n1= {n1}")
+print(f"After swap char2= {char2}")
+
+
+
+ 
