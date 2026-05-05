b842fe6b1462b527e66edb6b38de1f5fcf796623
Vikas5999 <vikasdon95@gmail.com>
2026-05-05 12:23:21 +0530

    update file

---
+++ 07-discount20%.py
@@ -0,0 +1,13 @@
+# Apply a 20% discount to a price 
+
+Before_price = 1000
+Discount = 20
+print(f"Before_price = {Before_price}")
+print(f"Discount = {Discount}")
+percentage = Before_price -(Before_price*Discount/100)
+print(f"After discount price {percentage}")
+
+
+
+
+ 
