7d5b2787d2205d73f0e0a7317d4ac8026df05bf8
Vikas5999 <vikasdon95@gmail.com>
2026-05-05 11:18:37 +0530

    update file

---
+++ 04-update-balance.py
@@ -0,0 +1,12 @@
+#5. Update balance after deposit 
+
+current_balance = 10000 
+deposit_balance = 5000
+
+print(f"current balance={current_balance}")
+print(f"deposit balance={deposit_balance}")
+
+current_balance=current_balance + deposit_balance
+print(f"After deposit current balance={current_balance}")
+
+
