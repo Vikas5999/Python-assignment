841d8848f0bd243f7a87fa768e9ea7e930e20887
Vikas5999 <vikasdon95@gmail.com>
2026-05-05 11:33:03 +0530

    update file

---
+++ 05-update-withdrawl.py
@@ -0,0 +1,10 @@
+#6. Update balance after withdrawal 
+
+balance = 12000 
+Withdrawl= 3000 
+
+print(f"Before balance={balance}")
+print(f"Withdrawl={Withdrawl}")
+Withdrawl =balance - Withdrawl
+print(f"After Withdrawl={Withdrawl}")
+
