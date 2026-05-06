ccb01bb0210e3fc5a307a72971ba075b4c6b2b2e
Vikas5999 <vikasdon95@gmail.com>
2026-05-06 14:20:07 +0530

    update file

---
+++ 08-student-percentage.py
@@ -0,0 +1,10 @@
+#9. Calculate student percentage
+
+obtain_marks = 430
+out_of = 500
+print(f"obtain_marks={obtain_marks}")
+print(f"out_of={out_of}")
+percentage=(obtain_marks/out_of)*100
+print(f"percentage={percentage}%")
+
+
