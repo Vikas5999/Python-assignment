2aafd90509692bf826a3f085b8da9e6612d2f6c8
Vikas5999 <vikasdon95@gmail.com>
2026-05-06 14:33:19 +0530

    update

---
+++ 09-average.py
@@ -0,0 +1,14 @@
+#10.Calculate total and average of 4 subjects
+
+Subject_1 = 80
+Subject_2 = 75
+Subject_3 = 90
+Subject_4 = 85
+print(f"Subject_1={Subject_1}")
+print(f"Subject_2={Subject_2}")
+print(f"Subject_3={Subject_3}")
+print(f"Subject_4={Subject_4}")
+Total_marks= Subject_1 + Subject_2 + Subject_3 + Subject_4
+print(f"Total_marks={Total_marks}")
+Average=Total_marks/4
+print(f"Average={Total_marks}")
