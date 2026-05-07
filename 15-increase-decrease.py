967ef6b84c6f2431aef09fbe9d6690a2e165ef40
Vikas5999 <vikasdon95@gmail.com>
2026-05-07 15:35:04 +0530

    update

---
+++ 15-increase-decrease.py
@@ -0,0 +1,10 @@
+# 16.Calculate percentage increase or decrease
+
+initial_value = 200
+final_value = 250
+print(f"initial_value = {initial_value}")
+print(f"final_value = {final_value}")
+
+change = final_value - initial_value
+increase = change/initial_value*100
+print(f"increase = {increase}")
