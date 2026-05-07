6b451373e2058622158d675a2b5dba337b427169
Vikas5999 <vikasdon95@gmail.com>
2026-05-07 14:29:48 +0530

    update

---
+++ 10-average1.py
@@ -0,0 +1,13 @@
+#11. Calculate average of 3 numbers
+Num1 = 25
+Num2 = 35
+Num3 = 45
+print(f"Num1={Num1}")
+print(f"Num2={Num2}")
+print(f"Num3={Num3}")
+Total_numbers = Num1 + Num2 + Num3
+print(f"Total_numbers={Total_numbers}")
+average = Total_numbers/3
+print(f"average={average}")
+      
+      
