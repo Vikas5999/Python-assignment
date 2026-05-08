d74f9769da3254c00026c3bfd840f91548099446
Vikas5999 <vikasdon95@gmail.com>
2026-05-08 11:31:25 +0530

    update

---
+++ 22-area-triangle.py
@@ -0,0 +1,8 @@
+# 21. Calculate area of a triangle
+
+base = 10
+height = 6
+print(f"base : {base}")
+print(f"height : {height}")
+Area = 0.5 * base * height
+print(f"Area : {Area}")
---
+++ 23-parameter-square.py
@@ -0,0 +1,6 @@
+# 22.Calculate perimeter of a square
+
+side = 9
+print(f"side : {side}")
+perimeter = 4 * side
+print(f"perimeter : {perimeter}")
