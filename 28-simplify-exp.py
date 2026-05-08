b659863f6bc7f30ae8bc30182dc83d1099771497
Vikas5999 <vikasdon95@gmail.com>
2026-05-08 11:57:11 +0530

    update

---
+++ 28-simplify-exp.py
@@ -0,0 +1,8 @@
+# 27.Simplify expression: x² - 4x + 4
+
+x = 3
+print(f"Original expression : x² - 4x + 4 ")
+print(f"value_of_x : {x}")
+simplified_expression = x**2 - 4*x + 4
+print(f"simplified_expression : {simplified_expression}")
+
