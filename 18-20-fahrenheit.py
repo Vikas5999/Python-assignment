ef6f5fa4091714a09c47affad7c9a8e1e38f0415
Vikas5999 <vikasdon95@gmail.com>
2026-05-08 11:14:21 +0530

    update

---
+++ 18-20-fahrenheit.py
@@ -0,0 +1,6 @@
+#18. Convert Celsius to Fahrenheit
+
+celsius = 20
+print(f"celsius = {celsius}")
+fahrenheit = (celsius * 9/5) + 32
+print(f"fahrenheit = {int(fahrenheit)}")
---
+++ 19-20-celcius.py
@@ -0,0 +1,6 @@
+
+
+fahrenheit = 20
+print(f"fahrenheit = {fahrenheit}")
+celsius = (fahrenheit - 32) *5/9 
+print(f"Celcius = {int(celsius)}")
