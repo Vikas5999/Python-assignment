1b679f108894367278375bd64dbc73eab1236c43
Vikas5999 <vikasdon95@gmail.com>
2026-05-08 11:45:06 +0530

    update

---
+++ 26-surface-area-cuboid.py
@@ -0,0 +1,12 @@
+# 25.Calculate surface area of a cuboid
+
+
+l = 4
+b = 3
+h = 2
+print(f"lenght : {l}")
+print(f"breadth : {b}")
+print(f"height : {h}")
+surface_area = 2 * (l*b + b*h + h*l)
+print(f"surface_area: {surface_area}")
+
